b84284d817dd207611c9e1da17441e7c5a8ca2d1
Mohamed Galal <mohamedjalalanwer@outlook.com>
2017-06-22 03:31:31 +0200

    upload

---
+++ Assets/Script/GameLogic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameLogic : MonoBehaviour {
+	public Text scoreText;
+	public static int  score ;
+	// Use this for initialization
+	void Start(){
+		score = 0;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		scoreText.text = "Score : " + score;
+	}
+}
---
+++ Assets/Script/StartNewGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartNewGame : MonoBehaviour {
+
+    
+
+	public void WaitAndPrint()
+    {
+        // suspend execution for 5 seconds
+        //yield return new WaitForSeconds(1);
+       //s print("WaitAndPrint " + Time.time);
+		// Application.LoadLevel("Game");
+		SceneManager.LoadScene ("Game");
+    }
+
+    
+}
+
